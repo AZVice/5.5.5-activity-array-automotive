85ba4b779c310d0efaf9cde605aed6c823a1dc49
Carlos Hernandez <chernandezmiranda@gmail.com>
2023-10-02 17:57:25 -0700

    added Mercury information

--- mercurySedan.js
+++ mercurySedan.js
@@ -1,12 +1,147 @@
 //this includes the vehicle class as a module
-const VehicleModule = require("./vehicleBaseClass")
+class Vehicle {
+    constructor(make, model, year, color, mileage) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.color = color;
+        this.passenger = 0;
+        this.speed = 0;
+        this.mileage = mileage;
+        this.started = false;
+        this.numberOfWheels = 0;
+        this.fuel = 0;
+    }
+
+    start() {
+        if (this.fuel > 0) {
+            return this.started = true;
+            console.log("engine started...!!!");
+        } else {
+            return this.started = false;
+            console.log("engine cannot start...");
+        }
+    }
+    accelerate() {
+        if (this.started) {
+            if (this.fuel > 0) {
+                console.log(this.speed += 1);
+                this.fuel = this.fuel - 1;
+            } else {
+                console.log("out of fuel.");
+                this.stop();
+            }
+        } else {
+            alert("You need to start the engine first.");
+        }
+    }
+    decelerate() {
+        if (this.started) {
+            if (this.fuel > 0) {
+                if (this.speed > 0) {
+                    console.log(this.speed -= 1);
+                    this.fuel = this.fuel - 1;
+                } else {
+                    console.log(this + " has stopped moving");
+                    this.fuel = this.fuel - 1;
+                }
+            } else {
+                console.log("out of fuel.");
+                this.stop();
+            }
+        } else {
+            alert("You need to start the engine first.");
+        }
+    }
+    stop() {
+        this.started = false;
+    }
+
+    drive() {
+        accelerate();
+    }
+    brake() {
+        decelerate();
+    }
+
+    autoPark()
+    {
+
+    }
+
+    autoDrive()
+    {
+      
+    }
+
+    typeOfVehicle(wheels) {
+        if (this.numberOfWheels == 8 && 8 == wheels) {
+            console.log(this.model + " " + this.make + " is a Truck");
+        } else if (this.numberOfWheels == 4 && 4 == wheels) {
+            console.log(this.model + " " + this.make + " is a CAr");
+        } else if (this.numberOfWheels == 2 && 2 == wheels) {
+            console.log(this.model + " " + this.make + " is a Bike");
+        } else {
+            console.log("Unknown type of vehicle");
+        }
+    }
+}
 
 //this shows how to call from this module...
-let v = new VehicleModule.Vehicle("Mercury", "Sedan", "1965", "color", "mileage");
+let v = new Vehicle("Mercury", "Sedan", "1965", "color", "mileage");
 console.log(v.make)
 
 
 //After you write the derived Car class, you should test it out.
+
+class Car extends Vehicle {
+    constructor(make, model, year, color, mileage){
+        super(make, model, year, color, mileage)
+        this.maximumPassengers = 5
+        this.numberOfWheels = 4
+        this.maximumSpeed = 160
+        this.fuel = 10
+        this.scheduleService = false
+}
+}
+
+
+
+
+this.loadPassenger(num)
+    if(this.passenger + num <= this.maximumPassengers){
+        console.log(this.model + " " + this.make + "is full")
+        return
+    }if(num + this.passenger > this.maximumPassengers){
+        console.log(this.model + " " + this.make + " it does not have enough seats")
+    }else {
+            this.passenger = this.passenger + num
+    }
+    return this.passenger
+
+
+   
+
+
+this.start()
+this.started = this.fuel > 0
+console.log(this.started ? "engine has started" : "No fuel")
+return this.started
+
+
+
+this.scheduleService(mileage)
+    return this.mileage > 30000
+    
+
+
+let testCar = new Car("Mercury", "A28", "1968", "green", "20")
+
+testCar.loadPassenger(5)
+testCar.start()
+testCar.scheduleService(30000)
+
+console.log(testCar.scheduleService())
 
 //Note: You can code your derived Car class here or make a file named index.js and do it there.
 
