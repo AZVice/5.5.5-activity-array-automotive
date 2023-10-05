39d032a97783beeb8ce3d63b5b35c84b7eff0c82
Carlos Hernandez <chernandezmiranda@gmail.com>
2023-10-05 09:04:05 -0700

    added all code to a single file for testing

--- mercurySedan.js
+++ mercurySedan.js
@@ -11,20 +11,23 @@ class Vehicle {
         this.started = false;
         this.numberOfWheels = 0;
         this.fuel = 0;
+
+
     }
 
-  /*  start() {
+    start() {
         if (this.fuel > 0) {
             console.log("engine started...!!!");
-            return this.started = true;
+            return this.started 
             
         } else {
             console.log("engine cannot start...");
-            return this.started = false;
+            this.started = false;
             
         }
+        return this.started;
     }
-*/
+
 
     accelerate() {
         if (this.started) {
@@ -64,10 +67,11 @@ class Vehicle {
 
 
     stop() {
+        console.log("Engine off")
         this.started = false;
     }
 
-    /*drive() {
+    drive() {
         accelerate();
     }
 
@@ -87,14 +91,14 @@ class Vehicle {
     {
       
     }
-*/
+
 
     typeOfVehicle() {
-        if (this.numberOfWheels == 8 && 8 == wheels) {
+        if (this.numberOfWheels == 8) {
             console.log(this.model + " " + this.make + " is a Truck");
-        } else if (this.numberOfWheels == 4 && 4 == wheels) {
+        } else if (this.numberOfWheels == 4) {
             console.log(this.model + " " + this.make + " is a CAr");
-        } else if (this.numberOfWheels == 2 && 2 == wheels) {
+        } else if (this.numberOfWheels == 2) {
             console.log(this.model + " " + this.make + " is a Bike");
         } else {
             console.log("Unknown type of vehicle");
@@ -113,61 +117,60 @@ console.log(v.make)*/
 
 //After you write the derived Car class, you should test it out.
 
-class Car extends Vehicle {
-    constructor(make, model, year, color, mileage){
-        super(make, model, year, color, mileage)
-        this.maximumPassengers = 5
-        this.numberOfWheels = 4
-        this.maximumSpeed = 160
-        this.fuel = 10
-        this.scheduleService = false
-}
-}
-
-
-
-
-this.loadPassenger(num)
-    if(this.passenger + num <= this.maximumPassengers){
-        console.log(this.model + " " + this.make + "is full")
-        return
-    }if(num + this.passenger > this.maximumPassengers){
-        console.log(this.model + " " + this.make + " it does not have enough seats")
-    }else {
-            this.passenger = this.passenger + num
-    }
-    return this.passenger
-
-
-   
-
-
-    start()
-    this.started = this.fuel > 0
-    console.log(this.started ? "engine has started" : "No fuel")
-    return this.started
-
-
-
-    scheduleService(mileage)
-        return this.mileage > 30000
-    
-
-
-    let testCar = new Car("Mercury", "A28", "1968", "green", 20)
-
-testCar.loadPassenger(5)
-testCar.start()
-testCar.scheduleService(30000)
-testCar.stop()
-
-console.log(testCar.scheduleService())
-
-//Note: You can code your derived Car class here or make a file named index.js and do it there.
-
 
 //TO DO: Code the Car subclass here or in index.js file, i.e. class Car extends Vehicle ...
 
+class Car extends Vehicle {
+    constructor(make, model, year, color, mileage){
+        super(make, model, year, color, mileage)
+        this.maximumPassengers = 5;
+        this.numberOfWheels = 4;
+        this.maximumSpeed = 160;
+        this.fuel = 10;
+        this.scheduleService = false;
+        this.passengerCount = 0;
+    }
+
+    checkService() { 
+        if (this.mileage > 30000){
+            this.needsService = true;
+            
+        }
+        return this.needsService
+    }
+        
+       
+    start(){
+       super.start();
+    } 
+
+
+
+    loadPassenger(num) {
+        if(this.passengerCount + num <= this.maximumPassengers){
+            this.passengerCount += num;
+            console.log(`loaded ${num} passengers. Passanger count ${this.passengerCount}`);
+            }else{
+                console.log(`${this.make} ${this.model} does not have enough room.`);
+    
+        }
+         
+    }
+
+}
+
+//Create at least two new instances of the Car class and test them here:
+let testCar = new Car("Mercury", "A28", "1968", "green", 20)
+console.log(testCar)
+
+let testCar2 = new Car("Ford", "GT", 2016, "Blue", 3500)
+console.log(testCar2)
+
+
+testCar.start()
+testCar.loadPassenger(5)
+testCar.stop()
+testCar.checkService(30000)
 
 
 
@@ -175,11 +178,38 @@ console.log(testCar.scheduleService())
 
 
 
+//Note: You can code your derived Car class here or make a file named index.js and do it there.
+class Truck extends Car{
+    constructor(make, model, year, color, mileage) {
+        super(make, model, year, color, mileage);
+        this.maximumPassengers = 3;
+        this.numberOfWheels = 8;
+        this.maximumSpeed = 160;
+        this.fuel = 10;
+        this.scheduleService = false;
+        this.passengerCount = 1;
+        this.carpetStrings = 40000;
+        this.cassetteDeck = true;
+        this.choke = true;
+        this.fuel = 0;
+        
+    }
+
+}
 
 
 
 
 //TO DO: Creating Instances and Testing Them
+
+let testTruck = new Truck("Toyoda", "T1000", 1925, "Green", 35000)
+console.log(testTruck)
+
+testTruck.start()
+testTruck.loadPassenger(1)
+testTruck.stop()
+testTruck.checkService(30000)
+testTruck.typeOfVehicle()
 
 //You can use the same instance "v" of the Vehicle class above for the base class.
 
@@ -187,4 +217,4 @@ console.log(testCar.scheduleService())
 
 
 
-//Create at least two new instances of the Car class and test them here:
+
